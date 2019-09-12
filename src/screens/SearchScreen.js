70781b06a408e017bd5f5514f713fb5fe83761ef
Daniel Ferolino <danielferolino@Daniels-MacBook-Pro.local>
2019-09-12 15:20:06 -0500

    initial commit

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,12 @@
+import React from 'React';
+import { View, Text, StyleSheets } from 'react-native';
+
+const SearchScreen = () => {
+    <View>
+        <Text>Search Screen</Text>
+    </View>
+};
+
+const styles = StyleSheet.create({
+    
+});
